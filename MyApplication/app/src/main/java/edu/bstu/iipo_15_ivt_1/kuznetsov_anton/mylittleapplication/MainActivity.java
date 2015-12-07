30bba2d8a0c61441780f4f4d5e7c1cfa813477c3
Anton332704 <Anton1Anton2Anton3@yandex.ru>
2015-12-07 22:33:22 +0300

    secAdd
    add

--- MyApplication/app/src/main/java/edu/bstu/iipo_15_ivt_1/kuznetsov_anton/mylittleapplication/MainActivity.java
+++ MyApplication/app/src/main/java/edu/bstu/iipo_15_ivt_1/kuznetsov_anton/mylittleapplication/MainActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView)findViewById(R.id.text);
     }
 
     @Override
